740e4a34d060d2ce1879e7b7affd5e7035202b3b
Bcjg23 <54872638+Bcjg23@users.noreply.github.com>
2020-10-24 17:17:24 -0500

    Update clean_data.py

--- python/clean_data.py
+++ python/clean_data.py
@@ -6,19 +6,6 @@ import pandas as pd
 import numpy as np
 
 
-# def clean_variables_names(data_frame):
-#     """
-#     Clean the name of the columns for the data_frame: lowercase, no spaces, no accent marks
-    
-#     param: data_frame to clean column names
-#     return: data_frame with clean colum names
-#     """
-    
-#     data_frame.columns = data_frame.columns.str.strip().str.lower().str.replace(' ','_')                  .str.replace('á','a').str.replace('é','e').str.replace('í','i')                      .str.replace('ó','o').str.replace('ú','u').str.replace('ñ','n')
-#     return data_frame
-
-
-
 def clean_variables_names(df):
     """
     Clean the name of the columns for the data_frame: lowercase, no spaces, no accent marks
@@ -36,14 +23,6 @@ def remove_whitespace(x):
         pass
     return x
 
-#def remove_especific_characters(x,chars):
-#    aux="[" + chars + "]"
-#    try:
-#        x=x.str.re.sub(aux,"",x)
-#     except:
-#         print("chalesss")
-#         pass
-#     return x
 
 def estandariza_datos(data):  
     data=pd.DataFrame(data)
